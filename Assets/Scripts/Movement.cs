780bb3088b8ebd728afb4922d9d4bd216a3a4d2d
nardoritos <30866958+nardoritos@users.noreply.github.com>
2018-06-07 22:46:53 -0300

    Time Gun - Omega 1.0.1
    - Mecânica de voltar no tempo adicionada
    - Organização de pastas
    - Deletar arquivos inúteis
    TODO
    - Comentar e organizar os códigos

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	private Timeback timeController;
+	public float movementSpeed;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		timeController = FindObjectOfType (typeof(Timeback)) as Timeback;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!timeController.isReversing) {
+			Mover ();
+			Pular ();
+		}
+    }
+
+    void Mover()
+    {
+        string animacao;
+        var anim = GetComponent<Animator>();
+        if (Input.GetAxis("Horizontal")==0)
+        {
+            
+            animacao = "Idle";
+           
+        }
+        else
+        {
+            animacao = "Walking";
+            
+        }
+        anim.Play(animacao);
+        
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0);
+		if(movement != Vector3.zero) 
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized * -1), 0.15f);
+	
+
+		transform.Translate (movement * movementSpeed * Time.deltaTime *-1, Space.World);
+       
+        
+    }
+
+	void Pular(){
+		if(Input.GetKeyDown(KeyCode.Space)){
+			rb.velocity= new Vector3(0,0,0);
+			rb.AddForce (Vector3.up * 1000f);
+			
+		}
+	}
+}
---
+++ Assets/Scripts/Timeback.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeback: MonoBehaviour {
+	public GameObject player;
+	public ArrayList keyframes;
+	public bool isReversing = false;
+
+	public int keyframe = 5;
+	private int frameCounter = 0;
+	private int reverseCounter = 0;
+
+	private Vector3 currentPosition;
+	private Vector3 previousPosition;
+	private Vector3 currentRotation;
+	private Vector3 previousRotation;
+
+
+	private bool firstRun = true;
+
+	void Start()
+	{
+		keyframes = new ArrayList();
+	}
+
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.Z))
+		{
+			isReversing = true;
+		}
+		else
+		{
+			isReversing = false;
+			firstRun = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if(!isReversing)
+		{
+			if(frameCounter < keyframe)
+			{
+				frameCounter += 1;
+			}
+			else
+			{
+				frameCounter = 0;
+				keyframes.Add(new KeyframeInGame(player.transform.position, player.transform.localEulerAngles));
+			}
+		}
+		else
+		{
+			if(reverseCounter > 0)
+			{
+				reverseCounter -= 1;
+			}
+			else
+			{
+				reverseCounter = keyframe;
+				RestorePositions();
+			}
+
+			if(firstRun)
+			{
+				firstRun = false;
+				RestorePositions();
+			}
+
+			float interpolation = (float) reverseCounter / (float) keyframe;
+			player.transform.position = Vector3.Lerp(previousPosition, currentPosition, interpolation);
+			player.transform.localEulerAngles = Vector3.Lerp(previousRotation, currentRotation, interpolation);
+		}
+
+		if(keyframes.Count > 128)
+		{
+			keyframes.RemoveAt(0);
+		}
+	}
+
+	void RestorePositions()
+	{
+		int lastIndex = keyframes.Count - 1;
+		int secondToLastIndex = keyframes.Count - 2;
+
+		if(secondToLastIndex >= 0)
+		{
+			currentPosition  = (keyframes[lastIndex] as KeyframeInGame).position;
+			previousPosition = (keyframes[secondToLastIndex] as KeyframeInGame).position;
+
+			currentRotation  = (keyframes[lastIndex] as KeyframeInGame).rotation;
+			previousRotation = (keyframes[secondToLastIndex] as KeyframeInGame).rotation;
+
+			keyframes.RemoveAt(lastIndex);
+		}
+	}
+}
+
+
+public class KeyframeInGame {
+	public Vector3 position;
+	public Vector3 rotation;
+
+	public KeyframeInGame(Vector3 position, Vector3 rotation)
+	{
+		this.position = position;
+		this.rotation = rotation;
+	}
+}
+
